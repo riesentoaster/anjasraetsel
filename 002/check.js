41dda56fbc38a999936c7241d177c09adb44bc11
Valentin Huber <valentinchuber@gmail.com>
2020-04-11 20:29:36 +0200

    resorted, added 002

---
+++ 002/check.js
@@ -0,0 +1,11 @@
+function check() {
+  var key = document.forms['form']['answer'].value;
+  var response;
+  if (key == "1559142843-5b8fcb9de0af") {
+    response =  "https://images.unsplash.com/photo-" + key + "?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80";
+  } else {
+    response = "not quite";
+  }
+  document.getElementsByTagName('a')[0].href = response;
+  return false;
+}
