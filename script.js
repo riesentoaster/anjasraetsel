546c9d4de8bb7737118579a779cfb3d4f4923eab
riesentoaster <valentinchuber+github@gmail.com>
2020-04-11 19:36:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function check(answer) {
+      document.getElementByID("response").innerHTML = "not quite";
   if (answer == "1559142843-5b8fcb9de0af") {
     document.getElementByID("response").innerHTML = "https://images.unsplash.com/photo-" + answer + "?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80";
   } else {
