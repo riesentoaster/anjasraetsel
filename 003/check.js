5dab66f6b277a279cb4753ce073a4753e98e5608
riesentoaster <valentinchuber+github@gmail.com>
2020-04-13 23:46:19 +0200

    removed error code as link

--- 003/check.js
+++ 003/check.js
@@ -4,7 +4,7 @@ function check() {
   if (key == "1559142843-5b8fcb9de0af") {
     response =  "https://images.unsplash.com/photo-" + key + "?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80";
   } else {
-    response = "not quite";
+    response = "";
   }
   document.getElementsByTagName('a')[0].href = response;
   return false;
